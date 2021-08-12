798a2f6695c0dab1cc6e6e766b969c4401feeb58
Diana Chacon <dchacon871@unab.edu.co>
2021-08-12 11:02:46 -0500

    Register and list users

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,14 @@ const cors = require("cors");
 const { dbConnection} = require("./db/db");
 require("dotenv").config();
 
+const User = require("./routes/user");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api/user", User);
+
+
 app.listen(process.env.PORT, () =>
 console.log("Backend server running on port: ", process.env.PORT)
 );
